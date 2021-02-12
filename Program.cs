90a92a0111205a4617d3d92c71a2ae627de8dbf6
Keyla <keylato96@gmail.com>
2021-02-11 21:39:38 -0700

    Se suben archivos del proyecto

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace InverseHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] array = new string[11] {"H","e","l","l","o","W","o","r","l","d","!"};
+
+            Console.WriteLine(array [10]);
+            Console.WriteLine(array [9]);
+            Console.WriteLine(array [8]);
+            Console.WriteLine(array [7]);
+            Console.WriteLine(array [6]);
+            Console.WriteLine(array [5]); 
+            Console.WriteLine(array [4]);
+            Console.WriteLine(array [3]);
+            Console.WriteLine(array [2]);
+            Console.WriteLine(array [1]);
+            Console.WriteLine(array [0]); 
+
+        }
+    }
+}
